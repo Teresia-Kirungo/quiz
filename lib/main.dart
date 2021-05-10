b4631cf93e6ac82937644e676e52d8f7782690d2
Teresia-Kirungo <terrykirungo@gmail.com>
2021-05-10 18:08:05 +0300

    Created quiz_brain that houses your QuestionBank

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:quiz/question.dart';
+import 'package:quiz/quiz_brain.dart';
 
+QuizBrain quizBrain = QuizBrain();
 void main() => runApp(
       Quiz(),
     );
@@ -31,14 +32,6 @@ class QuizPage extends StatefulWidget {
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [];
 
-  List<Question> questionBank = [
-    Question(q: 'You can lead a cow down stairs but not up stairs.', a: false),
-    Question(
-        q: 'Approximately one quarter of human bones are in the feet.',
-        a: true),
-    Question(q: 'A slug\'s blood is green.', a: true),
-  ];
-
   int questionNumber = 0;
   @override
   Widget build(BuildContext context) {
@@ -52,7 +45,8 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questionBank[questionNumber].questionText,
+                //tells it to look inside quizBrain > questionBank> questionNumber> questionText
+                quizBrain.questionBank[questionNumber].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -78,7 +72,7 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 bool correctAnswer =
-                    questionBank[questionNumber].questionAnswer;
+                    quizBrain.questionBank[questionNumber].questionAnswer;
                 if (correctAnswer == true) {
                   print('User got it right');
                 } else {
@@ -111,7 +105,7 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 bool correctAnswer =
-                    questionBank[questionNumber].questionAnswer;
+                    quizBrain.questionBank[questionNumber].questionAnswer;
                 if (correctAnswer == false) {
                   print('User got it right');
                 } else {
--- lib/question.dart
+++ lib/question.dart
@@ -1,10 +1,9 @@
+// This is a class
 class Question {
+  // This are methods which are just variables inside a class
   String questionText;
   bool questionAnswer;
 
-  //This is a Constructor
-  Question({String q, bool a}) {
-    questionText = q;
-    questionAnswer = a;
-  }
+  //This is a Constructor/methods which is a function inside a class
+  Question(questionText, questionAnswer);
 }
