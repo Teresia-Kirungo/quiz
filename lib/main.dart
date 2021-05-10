fe5f62fe2fdc85e9ef461a49ca1d94d9b9c12d2d
Teresia-Kirungo <terrykirungo@gmail.com>
2021-05-11 00:05:35 +0300

    Complete project

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'package:quiz/quiz_brain.dart';
+//TODO: Step 2 - Import the rFlutter_Alert package here.
+import 'package:rflutter_alert/rflutter_alert.dart';
+
+import 'quiz_brain.dart';
 
 QuizBrain quizBrain = QuizBrain();
+
 void main() => runApp(
       Quiz(),
     );
@@ -12,7 +15,7 @@ class Quiz extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.grey.shade800,
+        backgroundColor: Colors.grey.shade900,
         body: SafeArea(
           child: Padding(
             padding: EdgeInsets.symmetric(horizontal: 10.0),
@@ -32,7 +35,50 @@ class QuizPage extends StatefulWidget {
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [];
 
-  int questionNumber = 0;
+  void checkAnswer(bool userPickedAnswer) {
+    bool correctAnswer = quizBrain.getCorrectAnswer();
+
+    setState(() {
+      //TODO: Step 4 - Use IF/ELSE to check if we've reached the end of the quiz. If so,
+      //On the next line, you can also use if (quizBrain.isFinished()) {}, it does the same thing.
+      if (quizBrain.isFinished() == true) {
+        //TODO Step 4 Part A - show an alert using rFlutter_alert,
+
+        //This is the code for the basic alert from the docs for rFlutter Alert:
+        //Alert(context: context, title: "RFLUTTER", desc: "Flutter is awesome.").show();
+
+        //Modified for our purposes:
+        Alert(
+          context: context,
+          title: 'Finished!',
+          desc: 'You\'ve reached the end of the quiz.',
+        ).show();
+
+        //TODO Step 4 Part C - reset the questionNumber,
+        quizBrain.reset();
+
+        //TODO Step 4 Part D - empty out the scoreKeeper.
+        scoreKeeper = [];
+      }
+
+      //TODO: Step 6 - If we've not reached the end, ELSE do the answer checking steps below 👇
+      else {
+        if (userPickedAnswer == correctAnswer) {
+          scoreKeeper.add(Icon(
+            Icons.check,
+            color: Colors.green,
+          ));
+        } else {
+          scoreKeeper.add(Icon(
+            Icons.close,
+            color: Colors.red,
+          ));
+        }
+        quizBrain.nextQuestion();
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -45,8 +91,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                //tells it to look inside quizBrain > questionBank> questionNumber> questionText
-                quizBrain.questionBank[questionNumber].questionText,
+                quizBrain.getQuestionText(),
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -63,27 +108,14 @@ class _QuizPageState extends State<QuizPage> {
               child: Text('True'),
               style: TextButton.styleFrom(
                 primary: Colors.white,
-                backgroundColor: Colors.green,
-                onSurface: Colors.grey,
+                backgroundColor: Colors.red,
                 textStyle: TextStyle(
                   color: Colors.white,
                   fontSize: 20,
                 ),
               ),
               onPressed: () {
-                bool correctAnswer =
-                    quizBrain.questionBank[questionNumber].questionAnswer;
-                if (correctAnswer == true) {
-                  print('User got it right');
-                } else {
-                  print('User got it wrong');
-                }
-                setState(() {
-                  scoreKeeper.add(
-                    Icon(Icons.check, color: Colors.green),
-                  );
-                  questionNumber = questionNumber + 1;
-                });
+                checkAnswer(true);
                 //The user picked true.
               },
             ),
@@ -93,38 +125,22 @@ class _QuizPageState extends State<QuizPage> {
           child: Padding(
             padding: EdgeInsets.all(15.0),
             child: TextButton(
-              child: Text('False'),
+              child: Text('True'),
               style: TextButton.styleFrom(
                 primary: Colors.white,
-                backgroundColor: Colors.red,
-                onSurface: Colors.grey,
+                backgroundColor: Colors.green,
                 textStyle: TextStyle(
                   color: Colors.white,
                   fontSize: 20,
                 ),
               ),
               onPressed: () {
-                bool correctAnswer =
-                    quizBrain.questionBank[questionNumber].questionAnswer;
-                if (correctAnswer == false) {
-                  print('User got it right');
-                } else {
-                  print('User got it wrong');
-                }
-                setState(() {
-                  scoreKeeper.add(
-                    Icon(Icons.close, color: Colors.red),
-                  );
-                  questionNumber = questionNumber + 1;
-                });
-
+                checkAnswer(true);
                 //The user picked true.
               },
             ),
           ),
         ),
-
-        //TODO: Add a Row here as your score keeper
         Row(
           children: scoreKeeper,
         )
--- lib/question.dart
+++ lib/question.dart
@@ -1,9 +1,9 @@
-// This is a class
 class Question {
-  // This are methods which are just variables inside a class
   String questionText;
   bool questionAnswer;
 
-  //This is a Constructor/methods which is a function inside a class
-  Question(questionText, questionAnswer);
+  Question(String q, bool a) {
+    questionText = q;
+    questionAnswer = a;
+  }
 }
